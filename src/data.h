b13242a22863774a81ccb6814758c43f467ebbdd
Umbe-Exe <umbe.gt@libero.it>
2022-06-13 10:30:41 +0200

    specifying max radar ranges

--- src/data.h
+++ src/data.h
@@ -6,6 +6,8 @@
 #define battleBox {{0,0},{0.75,1}}
 #define botRadius 0.1f
 #define weaponRadius 0.02f
+#define radarMaxRange 0.5f
+#define rangeMaxRange 0.55f
 
 extern Bot *bots;
 extern uint8_t nOfBots;
