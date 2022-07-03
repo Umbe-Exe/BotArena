130e1f0eec34019541c01cf5d5847c739ade0c2c
Umbe-Exe <umbe.gt@libero.it>
2022-07-03 11:06:44 +0200

    added some constraint

--- src/competition.cpp
+++ src/competition.cpp
@@ -2,6 +2,12 @@
 #include "data.h"
 
 int addRadarGetId(int angle, int width, int range) {
+
+	if(width < 0) width *= -1;
+	if(range < 0) {
+		range *= -1;
+		angle += 180;
+	}
 	
 	currBot->sensor = (Sensor **)realloc(currBot->sensor, sizeof(Sensor *) * currBot->nOfSensors);
 
@@ -13,6 +19,11 @@ int addRadarGetId(int angle, int width, int range) {
 }
 
 int addRangeGetId(int angle, int range) {
+
+	if(range < 0) {
+		range *= -1;
+		angle += 180;
+	}
 	
 	currBot->sensor = (Sensor **)realloc(currBot->sensor, sizeof(Sensor*) * currBot->nOfSensors);
 
@@ -25,18 +36,24 @@ int addRangeGetId(int angle, int range) {
 
 void setSensorStatus(int sensorId, bool enabled) {
 
-	if(currBot->nOfSensors >= sensorId)
+	if(currBot->nOfSensors >= sensorId && sensorId >= 0)
 		currBot->sensor[sensorId]->enabled = 0;
 }
 
 int getSensorData(int sensorId) {
 
-	if(currBot->nOfSensors >= sensorId)
+	if(currBot->nOfSensors >= sensorId && sensorId >= 0)
 		return currBot->sensor[sensorId]->data;
 	else return INT_MAX;
 }
 
 void setMotorSpeed(int leftTread, int rightTread) {
+
+	if(leftTread > 100) leftTread = 100;
+	else if(leftTread < -100) leftTread = -100;
+	if(rightTread > 100) rightTread = 100;
+	else if(rightTread < -100) rightTread = -100;
+
 	currBot->leftTreadSpeed = leftTread;
 	currBot->rightTreadSpeed = rightTread;
 }
@@ -63,12 +80,24 @@ void setSystemChargeRate(System system, int rate) {
 
 	switch(system) {
 		case MISSILES:
+
+			if(currBot->shieldChargeRate + currBot->laserChargeRate + rate > 100) 
+				rate = 100 - currBot->shieldChargeRate + currBot->laserChargeRate;
+
 			currBot->missileChargeRate = rate;
 			break;
 		case SHIELDS:
+
+			if(currBot->missileChargeRate + currBot->laserChargeRate + rate > 100)
+				rate = 100 - currBot->missileChargeRate + currBot->laserChargeRate;
+
 			currBot->shieldChargeRate = rate;
 			break;
 		case LASERS:
+
+			if(currBot->missileChargeRate + currBot->shieldChargeRate + rate > 100)
+				rate = 100 - currBot->missileChargeRate + currBot->shieldChargeRate;
+
 			currBot->laserChargeRate = rate;
 			break;
 	}
