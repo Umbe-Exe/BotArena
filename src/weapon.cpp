2016bcf62e976f253e464ded914768dd065c5de6
Umberto <umbe.gt@libero.it>
2022-07-10 17:42:04 +0200

    fix indent

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -4,9 +4,9 @@
 void Missile::draw() {
 
 	al_draw_rotated_bitmap(missileBitmap,
-						   al_get_bitmap_width(missileBitmap) / 2,
-						   al_get_bitmap_height(missileBitmap) / 2,
-						   coord.x * getSmallestSide(), coord.y * getSmallestSide(), heading, 0);
+			       al_get_bitmap_width(missileBitmap) / 2,
+			       al_get_bitmap_height(missileBitmap) / 2,
+			       coord.x * getSmallestSide(), coord.y * getSmallestSide(), heading, 0);
 }
 
 void Missile::update(float delta) {}
@@ -14,9 +14,9 @@ void Missile::update(float delta) {}
 void Laser::draw() {
 
 	al_draw_rotated_bitmap(laserBitmap,
-						   al_get_bitmap_width(laserBitmap) / 2,
-						   al_get_bitmap_height(laserBitmap) / 2,
-						   coord.x * getSmallestSide(), coord.y * getSmallestSide(), heading, 0);
+			       al_get_bitmap_width(laserBitmap) / 2,
+			       al_get_bitmap_height(laserBitmap) / 2,
+			       coord.x * getSmallestSide(), coord.y * getSmallestSide(), heading, 0);
 }
 
 void Laser::update(float delta) {}
