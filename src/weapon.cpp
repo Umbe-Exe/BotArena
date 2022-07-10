0cc094f9ac46c71b2383c38663d15ae52b795c38
Umbe-Exe <umbe.gt@libero.it>
2022-07-10 10:33:35 +0200

    missile and laser draw

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -3,15 +3,20 @@
 
 void Missile::draw() {
 
-	al_draw_rotated_bitmap(missileBitmap, 
-						   al_get_bitmap_height(missileBitmap)/2,
+	al_draw_rotated_bitmap(missileBitmap,
+						   al_get_bitmap_height(missileBitmap) / 2,
 						   al_get_bitmap_width(missileBitmap) / 2,
-						   )
-
+						   coord.x, coord.y, heading, 0);
 }
 
 void Missile::update(float delta) {}
 
-void Laser::draw() {}
+void Laser::draw() {
+
+	al_draw_rotated_bitmap(laserBitmap,
+						   al_get_bitmap_height(laserBitmap) / 2,
+						   al_get_bitmap_width(laserBitmap) / 2,
+						   coord.x, coord.y, heading, 0);
+}
 
 void Laser::update(float delta) {}
