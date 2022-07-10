295a5020811b23067eff3e92ebeeb621e7b6bdb4
Umbe-Exe <umbe.gt@libero.it>
2022-07-10 10:50:55 +0200

    fix mistake to handle targets

--- src/bot.cpp
+++ src/bot.cpp
@@ -10,12 +10,22 @@ void Radar::priming(int sideLength) {
 	int half = w / 2;
 
 	bitmap = al_create_bitmap(w, w);
+
+	ALLEGRO_DISPLAY *target = al_get_current_display();
 	al_set_target_bitmap(bitmap);
 
 	al_draw_arc(half, half, half, RAD_PER_DEG * (angle - width / 2), RAD_PER_DEG * this->width, {color.r, color.g, color.b, 0.5f}, w);
+
+	al_set_target_backbuffer(target);
 }
 
-void Radar::draw() {}
+void Radar::draw() {
+
+	al_draw_rotated_bitmap(bitmap,
+						   al_get_bitmap_height(bitmap) / 2,
+						   al_get_bitmap_width(bitmap) / 2,
+						   currBot->coord.x, currBot->coord.y, currBot->heading + angle, 0);
+}
 
 void Radar::update(float delta) {}
 
