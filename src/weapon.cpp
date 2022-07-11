cc579799b27ea07a587998b0ad7c34377b68e951
Umbe-Exe <umbe.gt@libero.it>
2022-07-11 13:23:47 +0200

    partial logic implementation lacks use of destroyer see comments

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -9,7 +9,42 @@ void Missile::draw() {
 			       coord.x * getSmallestSide(), coord.y * getSmallestSide(), heading, 0);
 }
 
-void Missile::update(double delta) {}
+void Missile::update(double delta) {
+
+	for(uint8_t i = 0; i < nOfBots; ++i)
+		if(getDistance(coord, bots[i].coord) < botRadius + weaponRadius) {
+			if(bots[i].shield > shieldLeakLevel) bots[i].shield -= missileDamage;
+			else {
+				uint8_t generatorDamage = missileDamage - missileDamage / 100 * bots[i].shield;
+				bots[i].shield -= missileDamage - generatorDamage;
+				bots[i].energy -= generatorDamage;
+			}
+
+			if(bots[i].shield < 0) bots[i].shield = 0;
+			if(bots[i].energy <= 0); //subscribe bot to entity destroyer
+
+			for(uint8_t j = 0; j < nOfBots; ++j)
+				if(j != i && getDistance(coord, bots[j].coord) < missileBlastRadius + botRadius) {
+
+					uint8_t blastDamage = missileDamage - missileDamage * (getDistance(coord, bots[j].coord) / (missileBlastRadius + botRadius));
+
+					if(bots[i].shield > shieldLeakLevel)
+						bots[i].shield -= blastDamage;
+					else {
+						uint8_t generatorDamage = blastDamage - blastDamage / 100 * bots[i].shield;
+						bots[i].shield -= blastDamage - generatorDamage;
+						bots[i].energy -= generatorDamage;
+					}
+
+					if(bots[i].shield < 0) bots[i].shield = 0;
+					if(bots[i].energy <= 0); //subscribe bot to entity destroyer
+				}
+			//subscribe weapon to entity destroyer
+		}
+
+	coord.x += missileSpeed * cos(heading * RAD_PER_DEG);
+	coord.y += missileSpeed * sin(heading * RAD_PER_DEG);
+}
 
 void Laser::draw() {
 
@@ -19,4 +54,23 @@ void Laser::draw() {
 			       coord.x * getSmallestSide(), coord.y * getSmallestSide(), heading, 0);
 }
 
-void Laser::update(double delta) {}
+void Laser::update(double delta) {
+
+	for(uint8_t i = 0; i < nOfBots; ++i)
+		if(getDistance(coord, bots[i].coord) < botRadius + weaponRadius) {
+			if(bots[i].shield > shieldLeakLevel) bots[i].shield -= damage;
+			else {
+				uint8_t generatorDamage = damage - damage / 100 * bots[i].shield;
+				bots[i].shield -= damage - generatorDamage;
+				bots[i].energy -= generatorDamage;
+			}
+
+			if(bots[i].shield < 0) bots[i].shield = 0;
+			if(bots[i].energy <= 0); //subscribe bot to entity destroyer
+
+			//subscribe weapon to entity destroyer
+		}
+
+	coord.x += missileSpeed * cos(heading * RAD_PER_DEG);
+	coord.y += missileSpeed * sin(heading * RAD_PER_DEG);
+}
