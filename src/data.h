4553d6437a31dfed94a04006b83cef15b0ebfdbb
Umbe-Exe <umbe.gt@libero.it>
2022-07-11 13:27:11 +0200

    added data and changed blast radius

--- src/data.h
+++ src/data.h
@@ -11,10 +11,12 @@
 #define sensorEnergyConsumption 2
 
 #define laserDamageMoltiplicator 0.05f // already normalized on the 100% laser system level (100 * 0.05 = 5 max damage)
-#define missileBlastRadius 0.05f
+#define missileBlastRadius 0.1f
 #define missileDamage 10
 #define laserSpeed 0.15f
 #define missileSpeed 0.08f
+
+#define shieldLeakLevel 80
 
 #define bumpForce 0.05f
 #define friction 0.02f // friction applied after bump
