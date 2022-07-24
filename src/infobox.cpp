0451232dc8e1aad6164a84e7a6a3a5745a48fda8
Umbe-Exe <umbe.gt@libero.it>
2022-07-24 12:45:06 +0200

    infobox apparently completed

--- src/infobox.cpp
+++ src/infobox.cpp
@@ -13,6 +13,9 @@ float scrollPos = 0;
 
 int nameFontHeight;
 int sysFontHeight;
+int sysFontWidth;
+
+uint16_t bitmapX;
 
 void primeInfoboxBitmap() {
 
@@ -38,7 +41,9 @@ void primeInfoboxBitmap() {
 	bitmapWidth = al_get_bitmap_width(bitmap);
 	bitmapHeight = al_get_bitmap_height(bitmap);
 
-	float sysFontWidth = al_get_text_width(sysFont, "Generator");
+	bitmapX = win_w - bitmapWidth;
+
+	sysFontWidth = al_get_text_width(sysFont, "Generator");
 
 	for(uint8_t i = 0; i < nOfBots; ++i) {
 		al_draw_text(nameFont, bots[i]->color, bitmapWidth / 2.f, bitmapHeight / nOfBots * i, ALLEGRO_ALIGN_CENTER, (bots[i]->name ? bots[i]->name : "UNNAMED"));
@@ -47,7 +52,6 @@ void primeInfoboxBitmap() {
 		al_draw_text(sysFont, bots[i]->color, 0, nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight, 0, "Shield");
 		al_draw_text(sysFont, bots[i]->color, 0, nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 2.f, 0, "Missile");
 		al_draw_text(sysFont, bots[i]->color, 0, nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 3.f, 0, "Laser");
-
 
 		al_draw_rectangle(
 			sysFontWidth + sysFontWidth / 9.f,
@@ -91,7 +95,46 @@ void infoBoxScroll(float to) {
 void drawInfobox() {
 	al_draw_bitmap_region(bitmap, 0, scrollPos, bitmapWidth, win_h, win_w - bitmapWidth, 0, 0);
 
+	uint8_t start = 0, end = 0;
 
+	if(bitmapHeight < win_h) {
+		start = 0;
+		end = nOfBots;
+	} else {
+		start = scrollPos / (nameFontHeight + sysFontHeight * 4);
+		end = (scrollPos + win_h) / (nameFontHeight + sysFontHeight * 4);
+	}
+
+	for(uint8_t i = start; i < end; ++i) {
+
+		al_draw_filled_rectangle(
+			bitmapX + sysFontWidth + sysFontWidth / 9.f,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i - scrollPos,
+			bitmapX + sysFontWidth + sysFontWidth / 9.f + (bitmapWidth * 0.9f - sysFontWidth + sysFontWidth / 9.f) / bots[i]->energy,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight - scrollPos,
+			bots[i]->color);
+
+		al_draw_filled_rectangle(
+			bitmapX + sysFontWidth + sysFontWidth / 9.f,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight - scrollPos,
+			bitmapX + sysFontWidth + sysFontWidth / 9.f + (bitmapWidth * 0.9f - sysFontWidth + sysFontWidth / 9.f) / bots[i]->shield,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 2.f - scrollPos,
+			bots[i]->color);
+
+		al_draw_filled_rectangle(
+			bitmapX + sysFontWidth + sysFontWidth / 9.f,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 2.f - scrollPos,
+			bitmapX + sysFontWidth + sysFontWidth / 9.f + (bitmapWidth * 0.9f - sysFontWidth + sysFontWidth / 9.f) / bots[i]->missile,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 3.f - scrollPos,
+			bots[i]->color);
+
+		al_draw_filled_rectangle(
+			bitmapX + sysFontWidth + sysFontWidth / 9.f,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 3.f - scrollPos,
+			bitmapX + sysFontWidth + sysFontWidth / 9.f + (bitmapWidth * 0.9f - sysFontWidth + sysFontWidth / 9.f) / bots[i]->laser,
+			nameFontHeight + (nameFontHeight + sysFontHeight * 4) * i + sysFontHeight * 4.f - scrollPos,
+			bots[i]->color);
+	}
 }
 
 void destroyInfobox() {
