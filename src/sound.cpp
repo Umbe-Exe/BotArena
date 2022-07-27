91ce376cf6130a7f24242ec075d176e125a12728
Umbe-Exe <umbe.gt@libero.it>
2022-07-27 18:19:26 +0200

    better solution settings and added sound files

---
+++ src/sound.cpp
@@ -0,0 +1,45 @@
+#include "sound.h"
+#include <allegro5/allegro_acodec.h>
+
+ALLEGRO_SAMPLE *laserSound;
+ALLEGRO_SAMPLE *laserHitSound;
+ALLEGRO_SAMPLE *missileSound;
+ALLEGRO_SAMPLE *missileHitSound;
+ALLEGRO_SAMPLE *botCollisionSound;
+ALLEGRO_SAMPLE *botExplosionSound;
+
+void loadSounds() {
+	al_init_acodec_addon();
+	al_reserve_samples(100);
+
+	laserSound = al_load_sample("resources/sounds/laser.wav");
+	laserHitSound = al_load_sample("resources/sounds/laserhit.wav");
+	missileSound = al_load_sample("resources/sounds/missile.wav");
+	missileHitSound = al_load_sample("resources/sounds/missilehit.wav");
+	botCollisionSound = al_load_sample("resources/sounds/robotscollide.wav");
+	botExplosionSound = al_load_sample("resources/sounds/robotexplode.wav");
+}
+
+void playLaserSound() {
+	al_play_sample(laserSound, 1.f, 0.f, 1.f, ALLEGRO_PLAYMODE_ONCE, 0);
+}
+
+void playLaserHitSound() {
+	al_play_sample(laserHitSound, 1.f, 0.f, 1.f, ALLEGRO_PLAYMODE_ONCE, 0);
+}
+
+void playMissileSound() {
+	al_play_sample(missileSound, 0.2f, 0.f, 1.f, ALLEGRO_PLAYMODE_ONCE, 0);
+}
+
+void playMissileHitSound() {
+	al_play_sample(missileHitSound, 0.3f, 0.f, 1.f, ALLEGRO_PLAYMODE_ONCE, 0);
+}
+
+void playBotCollisionSound() {
+	al_play_sample(botCollisionSound, 0.2f, 0.f, 1.f, ALLEGRO_PLAYMODE_ONCE, 0);
+}
+
+void playBotExplosionSound() {
+	al_play_sample(botExplosionSound, 0.6f, 0.f, 1.f, ALLEGRO_PLAYMODE_ONCE, 0);
+}
---
+++ src/sound.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void loadSounds();
+
+void playLaserSound();
+void playLaserHitSound();
+void playMissileSound();
+void playMissileHitSound();
+void playBotCollisionSound();
+void playBotExplosionSound();
