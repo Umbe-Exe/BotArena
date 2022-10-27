00a5be63ea9c0b0c5978613034f2757e101a10b9
Umbe-Exe <umbe.gt@libero.it>
2022-10-27 12:36:58 +0200

    updated includes and project configuration

--- include/BotArena/Arena.h
+++ include/BotArena/Arena.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "competition.h"
 #ifndef ARENA_STATIC
     #ifdef _WIN32
         #ifdef EXPORTS
@@ -13,19 +14,26 @@
     #define ARENA_EXPORT
 #endif
 
-enum COLOR {
-	RED,
-	BLUE,
-	GREEN,
-	YELLOW,
-	RANDCOL
+class ARENA_EXPORT Arena {
+public:
+    Arena(const char *title = "Arena", const char *filename = "config.txt");
+    ~Arena();
+
+    void registerController(Controller *controller);
+
+    void start();
+
+    void appointManager(void (*manager)());
+    void onExitCallback(void (*onExit)());
+
+    void end();
+    void pause();
+    void unpause();
+    void enableFeedback();
+    void disableFeedback();
+    void setSpeed(double speed);
+
+private:
+    Arena_Impl *pimpl;
+    friend class Controller;
 };
-
-void ARENA_EXPORT start();
-void ARENA_EXPORT end();
-
-//manager() will be called every iteration of the game loop
-void ARENA_EXPORT appointManager(void (*manager)(), bool startPaused = 1);
-
-//The image background is considered as the color rgb(255, 0, 255)
-void ARENA_EXPORT registerBot(const char *name, COLOR color, const char *img, void (*updateFn)(double), void (*initFn)(), void (*deathSignal)() = nullptr);
--- include/BotArena/competition.h
+++ include/BotArena/competition.h
@@ -15,96 +15,128 @@
 
 //Feel free to help me by reporting bugs at https://github.com/Umbe-Exe/BotArena
 
+class Arena_Impl;
+
 enum System {
-	MISSILES, LASERS, SHIELDS
+    MISSILES, LASERS, SHIELDS
 };
 
 enum Armament {
-	MISSILE, LASER
+    MISSILE, LASER
 };
 
 enum BumpType {
-	WallHit = 1,
-	BotCollision = 2,
-	MissileHit = 4,
-	LaserHit = 8,
-	BlastWave = 16
+    WallHit = 1,
+    BotCollision = 2,
+    MissileHit = 4,
+    LaserHit = 8,
+    BlastWave = 16
+};
+
+enum COLOR {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    RANDCOL
 };
 
 //Coordinates always in range 0..1 and heading angle in degrees 360 
 struct GPSdata {
-	float x, y, heading;
+    float x, y, heading;
 };
 
-/******
-	Add a sensor with: 
-		angle: where to point it (360 degrees)
-		width: how wide you want it (360 degrees)
-		range: a percentage ofradarMaxRange
-		
-	Returns an ID to use with:
-		setSensorStatus()
-		getSensorData()
-*/
-int ARENA_EXPORT addRadarGetId(float angle, float width, int range);
+class ARENA_EXPORT Controller {
 
-/******
-	Add a sensor with:
-		angle: where to point it (360 degrees)
-		range: a percentage of rangeMaxRange
+public:
+    /******
+        Add a sensor with:
+            angle: where to point it (360 degrees)
+            width: how wide you want it (360 degrees)
+            range: a percentage ofradarMaxRange
 
-	Returns an ID to use with:
-		setSensorStatus()
-		getSensorData()
-*/
-int ARENA_EXPORT addRangeGetId(float angle, int range);
+        Returns an ID to use with:
+            setSensorStatus()
+            getSensorData()
+    */
+    int addRadarGetId(float angle, float width, float range);
 
-/*******
-	Each enabled sensor consumes sensorEnergyConsumption units of power over maxEnergy units.
-	Sensors are enabled by default.
+    /******
+        Add a sensor with:
+            angle: where to point it (360 degrees)
+            range: a percentage of rangeMaxRange
 
-		enabled: true to enable it, false to disable it.
-*/
-void ARENA_EXPORT setSensorStatus(int sensorId, bool enabled);
+        Returns an ID to use with:
+            setSensorStatus()
+            getSensorData()
+    */
+    int addRangeGetId(float angle, float range);
 
-/*******
-	Disabled sensors return -1.
-		
-		A radar returns a 1 when a robot is in its area.
-		A laser range returns a percentage of how close something is,
-			a wall or a bot, from 0 to 100, with 0 being nothing in sight.
+    /*******
+        Each enabled sensor consumes sensorEnergyConsumption units of power over maxEnergy units.
+        Sensors are enabled by default.
 
-*/
-int ARENA_EXPORT getSensorData(int sensorId);
+            enabled: true to enable it, false to disable it.
+    */
+    void setSensorStatus(int sensorId, bool enabled);
 
-//Values in range -100..100
-void ARENA_EXPORT setMotorSpeed(int leftTread, int rightTread);
+    /*******
+        Disabled sensors return -1.
 
-/******
-	Returns a percentage of the supply of energy, 0 to 100, with 100 being full supply.
-*/
-int ARENA_EXPORT getGeneratorOutput();
+            A radar returns a 1 when a robot is in its area.
+            A laser range returns a percentage of how close something is,
+                a wall or a bot, from 0 to 100, with 0 being nothing in sight.
 
-/******
-	Returns the level of the selected system (0 to 100)
-*/
-int ARENA_EXPORT getSystemEnergy(System system);
+    */
+    int getSensorData(int sensorId);
 
-/******
-	The sum of the charge rates can't exceed 100, so be careful.
-	You don't have to take into account the sensors consumption,
-		because it will be spread evenly in any case,
-		but you won't be penalized if you do, 
-		if you know the value of sensorEnergyConsumption and maxEnergy...
-*/
-void ARENA_EXPORT setSystemChargeRate(System system, int rate);
+    //Values in range -100..100
+    void setMotorSpeed(float leftTread, float rightTread);
 
-//Heading angle in degrees 360
-void ARENA_EXPORT fireWeapon(Armament weapon, float heading);
+    /******
+        Returns a percentage of the supply of energy, 0 to 100, with 100 being full supply.
+    */
+    int getGeneratorOutput();
 
-GPSdata ARENA_EXPORT getGPSdata();
+    /******
+        Returns the level of the selected system (0 to 100)
+    */
+    int getSystemEnergy(System system);
 
-/******
-	Check the bits specified in BumpType to know what has bumped into your robot
-*/
-char ARENA_EXPORT isBumping();
+    /******
+        The sum of the charge rates can't exceed 100, so be careful.
+        You don't have to take into account the sensors consumption,
+            because it will be spread evenly in any case,
+            but you won't be penalized if you do,
+            if you know the value of sensorEnergyConsumption and maxEnergy...
+    */
+    void setSystemChargeRate(System system, float rate);
+
+    //Heading angle in degrees 360
+    void fireWeapon(Armament weapon, float heading);
+
+    GPSdata getGPSdata();
+
+    /******
+        Check the bits specified in BumpType to know what has bumped into your robot
+    */
+    char isBumping();
+
+    virtual void onStart() = 0;
+    virtual void onUpdate(double delta) = 0;
+    virtual void onDeath() = 0;
+
+    virtual const char *name() {
+        return "unknown";
+    }
+    virtual COLOR color() {
+        return RANDCOL;
+    }
+    virtual const char *pathToImage() {
+        return 0;
+    }
+
+private:
+    Arena_Impl *arena;
+    friend class Arena_Impl;
+};
